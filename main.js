40b69512cffe955c8ea643e86f3886d2909bcb7c
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-04 10:25:55 +0200

    add variables

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // alert("Hello World!")
 
-alert('hello world')
 
 //writln ('hello world')
 
@@ -17,5 +16,19 @@ console.log(26)
 console.log(typeof 20)
 console.log(typeof 'giulio')
 
+const first_name = 'Andrea'
+
+console.log(first_name)
+
+
+//declarationand initialization
+
+let last_name = 'Meroi'
+
+console.log(last_name)
+
+
+last_name = 'Verdi'
+console.log(last_name)
 
 
