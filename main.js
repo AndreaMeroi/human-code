0666897a0e36fee90f64fe142dad62008373d82d
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-04 10:02:25 +0200

    Add live exercise

---
+++ main.js
@@ -0,0 +1,19 @@
+// alert("Hello World!")
+
+//writln ('hello world')
+
+
+document.writeln('hello world')
+
+//getElementById.writln ('hello world')
+
+document.getElementById('title').innerHTML = 'hello world'
+
+console.log("Marco")
+console.log(26)
+
+console.log(typeof 20)
+console.log(typeof 'giulio')
+
+
+
