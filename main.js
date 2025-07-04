1ae4c9f8b37d6ac99e0974c84b3db9d5d25908d3
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-04 11:43:55 +0200

    add final live coding

--- main.js
+++ main.js
@@ -31,4 +31,47 @@ console.log(last_name)
 last_name = 'Verdi'
 console.log(last_name)
 
+//writln + variabile
 
+document.writeln(last_name)
+
+
+//concatenazione ''+''
+
+console.log('Ciao ' + last_name)
+
+//interpolazionecon backtick ``
+
+console.log(`
+
+    Andrea
+
+
+    ${last_name}
+
+Andrea
+    
+    `)
+
+//numbers
+
+
+
+const sum = 4 + 3
+console.log(sum)
+
+
+const result = sum / 4
+console.log(result)
+
+const multi = result * 25
+console.log(multi)
+
+const subs = result - multi
+console.log(subs)
+
+// accesso alle proprietà e metodi usando la dot notation 
+
+console.log(20.282982982982.toFixed(3))
+console.log(last_name.length)
+console.log(last_name.split(''))
